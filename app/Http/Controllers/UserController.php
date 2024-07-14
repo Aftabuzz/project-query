e7061733678dc2d4b9d0fb12968206c4d922fc7a
Aftabuzz <aftabuzz020@gmail.com>
2024-07-15 01:49:15 +0600

    insert & update dynamic code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -50,4 +50,188 @@ class UserController extends Controller
     return view('user',['data' => $user]);
 
     }
+
+    public function adduser(Request $req){
+
+      $user =DB::table('users')
+                     // ->insertOrIgnore([                     //check duplicate value
+                      //  ->insertGetId([
+                        ->insert([
+ 
+                        //->upsert([                         //update code
+                          'name' => $req->username,
+                          'email' => $req->useremail,
+                          'age' => $req->userage,
+                          'city' => $req->usercity,
+                          'created_at' => now(),
+                          'updated_at' => now()
+
+                      ]
+                          //['email'],                                //update code
+                          //['city']                                  //only city name will update 
+                          );
+                         // return $user;
+                               if($user){
+                                return redirect()->route('home');
+                             //echo"<h2>Data inserted successfully.</h2>";
+
+                            }    else{
+                                echo "<h2>Data not added</h2>";         //incase duplicate value
+
+                  }
+                 
+    }
+    // public function updateUser(){
+    //   $user =DB::table('users')
+    //   ->where('id',2)
+    //   ->update([
+    //      'city' => 'Sydny',
+    //      'age' => 20
+  //      ]);
+
+
+    // public function updateUser(){
+    //   $user =DB::table('users')
+    //            ->updateOrinsert(                               //condition update
+    //            [
+    //             'email' => 'aftab@gmail.com',
+    //             'name' => 'Aftab'
+    //            ],
+
+    //            [
+    //             'age' => 20
+    //            ]
+
+    //               );
+
+      
+    //   if($user){
+    //                echo"<h2>Data updated successfully.</h2>";
+
+    //               }    else{
+    //                   echo "<h2>Data not updated</h2>";        
+
+    //     }
+
+    //   }
+
+
+      // public function updateUser(){
+      //   $user =DB::table('users')
+      //            ->updateOrinsert(                //update new data after cheking exit or not                         
+      //            [
+      //             'email' => 'axyz@gmail.com',
+      //             'name' => 'xyz',
+      //             'city' => 'hawaii'
+      //            ],
+  
+      //            [
+      //             'age' => 21
+      //            ]
+  
+      //               );
+  
+        
+      //   if($user){
+      //                echo"<h2>Data updated successfully.</h2>";
+  
+      //               }    else{
+      //                   echo "<h2>Data not updated</h2>";        
+  
+      //     }
+
+      public function updatePage(string $id){
+
+        //$user = DB::table('users')->where('id',$id)->get();
+        $user = DB::table('users')->find($id);
+        return view('updateuser',['data' => $user]);
+         //return $user;
+
+      }
+
+
+        //   public function updateUser(){                   //increment/decrement age every visit
+        //     $user =DB::table('users')
+        //              ->where('id', 3)
+        //              ->increment('age');
+        //              //->increment('age', 3);
+        //              //->decrement('age');
+               
+      
+            
+        //     if($user){
+        //                  echo"<h2>Data updated successfully.</h2>";
+      
+        //                 }    else{
+        //                     echo "<h2>Data not updated</h2>";        
+      
+        //       }
+      
+  
+        // }
+
+
+        // public function updateUser(){                   
+        //   $user =DB::table('users')
+        //            ->where('id', 3)
+                   
+        //            ->increment('age', 3, ['city' => 'New York']);
+                
+             
+    
+          
+        //   if($user){
+        //                echo"<h2>Data updated successfully.</h2>";
+    
+        //               }    else{
+        //                   echo "<h2>Data not updated</h2>";        
+    
+        //     }
+    
+            public function updateUser(Request $req, $id){          //increate multiple column                                   
+              $user =DB::table('users')
+                       ->where('id', $id)
+                       ->update([
+                      // ->incrementEach([
+                           'name' => $req->username,
+                          'email' => $req->useremail,
+                          'age' => $req->userage,
+                          'city' => $req->usercity,
+                         //  'votes' => 1
+
+                       ]);
+ 
+              if($user){
+                return redirect()->route('home');
+                           //echo"<h2>Data updated successfully.</h2>";
+        
+                          }    else{
+                              echo "<h2>Data not updated</h2>";        
+        
+                }
+
+
+      }
+
+      public function deleteUser(string $id){
+        $user =DB::table('users')
+        ->where('id', $id)
+        ->delete();
+         
+        if($user){
+            return redirect()->route('home');
+
+        }
+
+      } 
+
+      public function deleteAllUser(){
+
+        $user = DB::table('users')
+                   ->truncate();
+      }
+
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,25 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+Route::controller(UserController::class)->group(function(){
 
-Route::get('/', [UserController::class, 'showUsers']);
+    Route::get('/', 'showUsers')->name('home');
 
-Route::get('/user/{id}', [UserController::class, 'singleUser'])->name('view.user');         //view users
+    Route::get('/user/{id}',  'singleUser')->name('view.user');         //view users
+    
+    Route::post('/add',  'addUser')->name('addUser');
+    
+    Route::post('/update/{id}',  'updateUser')->name('update.user');
+    
+    Route::get('/updatepage/{id}', 'updatePage')->name('update.page');
+    
+    Route::get('/delete/{id}', 'deleteUser')->name('delete.user');
+    
+    Route::get('/deleteuser', 'deleteAllUser');
+
+
+});
+
+
+
+Route::view('newuser', '/adduser');
